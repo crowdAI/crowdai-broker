9675ff5ec5b4e9fa3bd5a5f6afb09b4dbe1b0649
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-08 12:34:30 +0200

    Make socketio host and port configurable

--- run.py
+++ run.py
@@ -186,4 +186,4 @@ def execute_function(args):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host=config['SOCKETIO-HOST'], port=config['SOCKETIO-PORT'])
